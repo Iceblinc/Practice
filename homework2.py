b2167faad7ee7c0b77370800e8bc4d13e8084040
Iceblinc <sitiente.scientia@gmail.com>
2024-07-07 12:54:51 +0300

    Update homework2.py

---
+++ homework2.py
@@ -0,0 +1,10 @@
+The_number_of_completed_DZ = 12
+a = The_number_of_completed_DZ
+Number_of_hours_spent = 1.5
+b = Number_of_hours_spent
+Course_name = "Python"
+c = Course_name
+Time_for_one_task = (b / a)
+g = Time_for_one_task
+
+print("Курс:", c ,", всего задач:", a , "затрачено часов:", b , "среднее время выполнения:", g )
