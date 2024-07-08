3aeb6dcffc4e5219fdb81476bf5821df8572bf0d
Iceblinc <sitiente.scientia@gmail.com>
2024-07-08 18:50:38 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,6 +1,6 @@
 immutable_var = "String", 55, 5.24, True
 print(immutable_var)
-# immutable_var[0] = "Doom"
+immutable_var[0] = "Doom"
 # После создания кортежа нельзя изменить его элементы, добавить новые элементы или удалить существующие.
 
 mutable_list = [1, 2, 3, "Привет", True]
